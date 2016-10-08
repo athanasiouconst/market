221787e66187900647cf9b5d91202229a88c4e25
costas <costas@DESKTOP-GB1ASLH>
2016-10-08 21:00:18 +0300

    commit crud procedure

---
+++ application/views/prefooter/prefooter.php
@@ -0,0 +1,100 @@
+<!-- follow-us -->
+	<div class="follow-us">
+		<div class="container">
+			<div class="agileinfo_follow_us_grid">
+				<div class="agileits_follow_us_left">
+					<h3>Follow Us On :</h3>
+				</div>
+				<div class="agileits_follow_us_right">
+					<ul class="w3_agileits_social_icons">
+						<li><a href="#" class="wthree_facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="wthree_twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="wthree_google"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="wthree_rss"><i class="fa fa-rss" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="wthree_linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="wthree_dribble"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="wthree_instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="wthree_utube"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="wthree_tumblr"><i class="fa fa-tumblr" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="agile_stocks_mutual_funds">
+				<div class="agile_stocks_mutual_fund_grid">
+					<div class="agile_stocks_mutual_fund_grid_l">
+						<h3>Stocks :</h3>
+					</div>
+					<div class="agile_stocks_mutual_fund_grid_r">
+						<ul>
+							<li><a href="single.html">a</a><i>|</i></li>
+							<li><a href="single.html">b</a><i>|</i></li>
+							<li><a href="single.html">c</a><i>|</i></li>
+							<li><a href="single.html">d</a><i>|</i></li>
+							<li><a href="single.html">e</a><i>|</i></li>
+							<li><a href="single.html">f</a><i>|</i></li>
+							<li><a href="single.html">g</a><i>|</i></li>
+							<li><a href="single.html">h</a><i>|</i></li>
+							<li><a href="single.html">i</a><i>|</i></li>
+							<li><a href="single.html">j</a><i>|</i></li>
+							<li><a href="single.html">k</a><i>|</i></li>
+							<li><a href="single.html">l</a><i>|</i></li>
+							<li><a href="single.html">m</a><i>|</i></li>
+							<li><a href="single.html">n</a><i>|</i></li>
+							<li><a href="single.html">o</a><i>|</i></li>
+							<li><a href="single.html">p</a><i>|</i></li>
+							<li><a href="single.html">q</a><i>|</i></li>
+							<li><a href="single.html">r</a><i>|</i></li>
+							<li><a href="single.html">s</a><i>|</i></li>
+							<li><a href="single.html">t</a><i>|</i></li>
+							<li><a href="single.html">u</a><i>|</i></li>
+							<li><a href="single.html">v</a><i>|</i></li>
+							<li><a href="single.html">w</a><i>|</i></li>
+							<li><a href="single.html">x</a><i>|</i></li>
+							<li><a href="single.html">y</a><i>|</i></li>
+							<li><a href="single.html">z</a><i>|</i></li>
+							<li><a href="single.html">Others</a></li>
+						</ul>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="agile_stocks_mutual_fund_grid">
+					<div class="agile_stocks_mutual_fund_grid_l">
+						<h3>mutual funds :</h3>
+					</div>
+					<div class="agile_stocks_mutual_fund_grid_r">
+						<ul>
+							<li><a href="single.html">a</a><i>|</i></li>
+							<li><a href="single.html">b</a><i>|</i></li>
+							<li><a href="single.html">c</a><i>|</i></li>
+							<li><a href="single.html">d</a><i>|</i></li>
+							<li><a href="single.html">e</a><i>|</i></li>
+							<li><a href="single.html">f</a><i>|</i></li>
+							<li><a href="single.html">g</a><i>|</i></li>
+							<li><a href="single.html">h</a><i>|</i></li>
+							<li><a href="single.html">i</a><i>|</i></li>
+							<li><a href="single.html">j</a><i>|</i></li>
+							<li><a href="single.html">k</a><i>|</i></li>
+							<li><a href="single.html">l</a><i>|</i></li>
+							<li><a href="single.html">m</a><i>|</i></li>
+							<li><a href="single.html">n</a><i>|</i></li>
+							<li><a href="single.html">o</a><i>|</i></li>
+							<li><a href="single.html">p</a><i>|</i></li>
+							<li><a href="single.html">q</a><i>|</i></li>
+							<li><a href="single.html">r</a><i>|</i></li>
+							<li><a href="single.html">s</a><i>|</i></li>
+							<li><a href="single.html">t</a><i>|</i></li>
+							<li><a href="single.html">u</a><i>|</i></li>
+							<li><a href="single.html">v</a><i>|</i></li>
+							<li><a href="single.html">w</a><i>|</i></li>
+							<li><a href="single.html">x</a><i>|</i></li>
+							<li><a href="single.html">y</a><i>|</i></li>
+							<li><a href="single.html">z</a></li>
+						</ul>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- //follow-us -->
