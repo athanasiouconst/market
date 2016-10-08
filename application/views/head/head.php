7bacf37bf0cbbc4ec16f80b4f35e3c50cbee16be
costas <costas@DESKTOP-GB1ASLH>
2016-10-08 21:00:14 +0300

    commit crud procedure

---
+++ application/views/head/head.php
@@ -0,0 +1,60 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!--
+author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Trade Market a Corporate Business Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Trade Market Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="<?php echo base_url(); ?>css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="<?php echo base_url(); ?>css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link class="include" rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/jquery.jqplot.css" />
+<!-- calender -->
+<link type="text/css" href="<?php echo base_url(); ?>css/jquery.simple-dtpicker.css" rel="stylesheet" />
+<!-- //calender -->
+<!-- different-chart-bar -->
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/chart.min.css">
+<!-- //different-chart-bar -->
+<!-- font-awesome icons -->
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/font-awesome.min.css" />
+<!-- //font-awesome icons -->
+<!-- js -->
+<script src="<?php echo base_url(); ?>js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.marquee.min.js"></script>
+<!-- js -->
+<!-- pop-up -->
+<link href="<?php echo base_url(); ?>css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //pop-up -->
+<!-- left-chart -->
+<script src="<?php echo base_url(); ?>js/jquery.flot.min.js" type="text/javascript"></script> 
+<script src="<?php echo base_url(); ?>js/jquery.flot.animator.min.js" type="text/javascript"></script>
+<!-- //left-chart -->
+<link href="//fonts.googleapis.com/css?family=Muli:300,300i,400,400i" rel="stylesheet">
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="<?php echo base_url(); ?>js/move-top.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/table.css">
+
+<!-- start-smoth-scrolling -->
+</head>
